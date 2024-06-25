830f1f23ef91051a4266ee2acbdd1969523f1a11
Adrian Rubio-Punal <adrian.rubio.punal@gmail.com>
2024-06-25 12:44:28 +0200

    Minor update

--- git_auto_commit.sh
+++ git_auto_commit.sh
@@ -23,3 +23,26 @@ if [ -n "$(git status --porcelain)" ]; then
 else
     logger "No changes to commit"
 fi
+
+cd /home/adrian/Documents/Perceptron
+
+git pull
+
+# Check for changes
+if [ -n "$(git status --porcelain)" ]; then
+
+    git status
+
+    # Stage all changes
+    git add -A .
+
+    # Commit with a generic message
+    git commit -m "Auto commit on $(date)"
+
+    # Push changes to GitHub
+    git push origin main
+
+    logger "Pushed to github correctly"
+else
+    logger "No changes to commit"
+fi
